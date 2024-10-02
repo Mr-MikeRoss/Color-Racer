ac3a3b881283be672ffd49401da1c2c32152a609
Bread <jmagri1@uncc.edu>
2024-10-02 15:28:51 -0400

    Timer stops when cars finish, cars only move if when condition is not met, yet

--- assets/js/game-logic.js
+++ assets/js/game-logic.js
@@ -5,6 +5,7 @@ const player2Time = document.getElementById("player2-time");
 const startGame = document.getElementById("start-button");
 const scoreGame = document.getElementById("save-score");
 const speed = 10;
+const playGame = true;
 let player1Interval;
 let player2Interval;
 let position1 = 0;
@@ -12,26 +13,33 @@ let position2 = 0;
 let keyIsPressed = false;
 let count1 = 0;
 let count2 = 0;
-// win condition is count = 106 ~ 127;
+// win condition is count = 126
 //  --------------------- global var box ----------------------------------------
 
-// Move car functions
-
+// Move car functions only allow movement if respective counts are <= 126.
 function moveCar1() {
-  position1 += speed;
-  playerOneCar.style.left = position1 + "px";
-  count1++;
-  console.log(count1);
+  if (count1 <= 126) {
+    position1 += speed;
+    playerOneCar.style.left = position1 + "px";
+    count1++;
+    console.log(count1);
+  } else {
+    clearInterval(player1Interval);
+  }
 }
 
 function moveCar2() {
-  position2 += speed;
-  playerTwoCar.style.left = position2 + "px";
-  count2++;
-  console.log(count2);
+  if (count2 <= 126) {
+    position2 += speed;
+    playerTwoCar.style.left = position2 + "px";
+    count2++;
+    console.log(count2);
+  } else {
+    clearInterval(player2Interval);
+  }
 }
 
-// timer function
+// timer function updates player times on screen.
 function timeHandler() {
   let timeCount1 = 0;
   player1Interval = setInterval(function timeInterval() {
@@ -47,49 +55,46 @@ function timeHandler() {
   }, 1000);
 }
 
-// gameLoop
+// Enables event listers while true, 
 function gameLoop() {
-  // -------------game variables------------------------
   // keyPress event listeners
-  if (count1 < 126 && count2 < 126) {
-    document.addEventListener("keydown", function (event) {
-      if (event.key === "a" && !keyIsPressed) {
-        keyIsPressed = true;
-        moveCar1();
-      }
-    });
+  if (playGame) {
+    if (count1 < 126 && count2 < 126) {
+      document.addEventListener("keydown", function (event) {
+        if (event.key === "a" && !keyIsPressed) {
+          keyIsPressed = true;
+          moveCar1();
+        }
+      });
 
-    document.addEventListener("keyup", function (event) {
-      if (event.key === "a") {
-        keyIsPressed = false;
-      }
-    });
+      document.addEventListener("keyup", function (event) {
+        if (event.key === "a") {
+          keyIsPressed = false;
+        }
+      });
 
-    document.addEventListener("keydown", function (event) {
-      if (event.key === "l" && !keyIsPressed) {
-        keyIsPressed = true;
-        moveCar2();
-      }
-    });
+      document.addEventListener("keydown", function (event) {
+        if (event.key === "l" && !keyIsPressed) {
+          keyIsPressed = true;
+          moveCar2();
+        }
+      });
 
-    document.addEventListener("keyup", function (event) {
-      if (event.key === "l") {
-        keyIsPressed = false;
-      }
-    });
-  } else if (count1 === 126) {
-    clearInterval(player1Interval);
-  } else if (count2 === 126) {
-    clearInterval(player2Interval);
-  } else if (count1 === 126 && count2 === 126) {
-    return;
+      document.addEventListener("keyup", function (event) {
+        if (event.key === "l") {
+          keyIsPressed = false;
+        }
+      });
+    }
+    //Game over condition.
+    if (count1 >= 126 && count2 >= 126) {
+      playGame = false;
+      console.log("Game over.");
+    }
   }
-  // timer function event listener.
-
 }
 
-
-// game event listener.
+//Game event listener.
 startGame.addEventListener("click", function () {
   timeHandler();
   gameLoop();
