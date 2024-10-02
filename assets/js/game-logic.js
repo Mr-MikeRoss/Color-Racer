7e3c5f8a37d4c33f5f25b077daef84676090d5c1
Bread <jmagri1@uncc.edu>
2024-10-02 15:02:03 -0400

    test

--- assets/js/game-logic.js
+++ assets/js/game-logic.js
@@ -7,7 +7,8 @@ const scoreGame = document.getElementById("save-score");
 const speed = 10;
 let player1Interval;
 let player2Interval;
-let position = 0;
+let position1 = 0;
+let position2 = 0;
 let keyIsPressed = false;
 let count1 = 0;
 let count2 = 0;
@@ -18,15 +19,15 @@ let playGame = true;
 // Move car functions
 
 function moveCar1() {
-  position += speed;
-  playerOneCar.style.left = position + "px";
+  position1 += speed;
+  playerOneCar.style.left = position1 + "px";
   count1++;
   console.log(count1);
 }
 
 function moveCar2() {
-  position += speed;
-  playerTwoCar.style.left = position + "px";
+  position2 += speed;
+  playerTwoCar.style.left = position2 + "px";
   count2++;
   console.log(count2);
 }
@@ -50,44 +51,38 @@ function timeHandler() {
 // gameLoop
 function gameLoop() {
   // -------------game variables------------------------
-
   // keyPress event listeners
   document.addEventListener("keydown", function (event) {
+    
     if (event.key === "a" && !keyIsPressed) {
       keyIsPressed = true;
       moveCar1();
     }
   });
+}
 
-  document.addEventListener("keyup", function (event) {
-    if (event.key === "a") {
-      keyIsPressed = false;
-    }
-  });
-
-  document.addEventListener("keydown", function (event) {
-    if (event.key === "l" && !keyIsPressed) {
-      keyIsPressed = true;
-      moveCar2();
-    }
-  });
-
-  document.addEventListener("keyup", function (event) {
-    if (event.key === "l") {
-      keyIsPressed = false;
-    }
-  });
-
-  // timer function event listener.
-  if (count1 === 126) {
-    clearInterval(player1Interval);
-    //pause timer, say player 1 won
-  } else if (count2 === 126) {
-    clearInterval(player2Interval);
-    //pause timer, say player 2 won
-  } else if (count1 === 126 && count2 === 126) {
-    playGame = false;
+document.addEventListener("keyup", function (event) {
+  if (event.key === "a") {
+    keyIsPressed = false;
   }
+});
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "l" && !keyIsPressed) {
+    keyIsPressed = true;
+  }
+  moveCar2();
+});
+
+document.addEventListener("keyup", function (event) {
+  if (event.key === "l") {
+    keyIsPressed = false;
+  }
+});
+
+// game end condition.
+if (count1 === 126 && count2 === 126) {
+  playGame = false;
 }
 
 // game event listener.
