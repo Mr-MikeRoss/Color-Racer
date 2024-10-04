5f23491e47db71519d2522d8f868e735ec78c31d
midknight <mrmikeross1995@gmail.com>
2024-10-03 20:33:03 -0400

    Minor changes

--- assets/js/game-logic.js
+++ assets/js/game-logic.js
@@ -46,22 +46,22 @@ function timeHandler() {
     timeCount1++;
     player1Time.textContent = `${timeCount1} sec`;
     player1Time.style = "font-size:xx-large";
-    if (window.matchMedia("(max-width: 375px)").matches) {
-      player1Time.style = "font-size: 14px";
-    }
     if (window.matchMedia("(max-width: 768px)").matches) {
       player1Time.style = "font-size: 20px";
+    }
+    if (window.matchMedia("(max-width: 375px)").matches) {
+      player1Time.style = "font-size: 14px";
     }
   }, 1000);
   player2Interval = setInterval(function timeInterval() {
     timeCount2++;
     player2Time.textContent = `${timeCount2} sec`;
     player2Time.style = "font-size:xx-large";
-    if (window.matchMedia("(max-width: 375px)").matches) {
-      player2Time.style = "font-size: 14px";
-    }
     if (window.matchMedia("(max-width: 768px)").matches) {
       player2Time.style = "font-size: 20px";
+    }
+    if (window.matchMedia("(max-width: 375px)").matches) {
+      player2Time.style = "font-size: 14px";
     }
   }, 1000);
 }
